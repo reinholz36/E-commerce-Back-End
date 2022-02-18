e4141d3d33fae17209ce72cd4d2616dd41877ec8
reinholz36 <dreinholz28@gmail.com>
2022-02-18 16:07:27 -0500

    added model fields and rules for category, product, product tag, tag, and index

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,5 @@
 // import important parts of sequelize library
+const { STRING } = require('mysql2/lib/constants/types');
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
@@ -9,7 +10,35 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false, 
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Category',
+        key: 'id'
+      },
+    },
   },
   {
     sequelize,
